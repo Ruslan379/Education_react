8cd3f323db9de8917dfce4848a923394ea560f92
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-29 21:37:17 +0300

    Work on cm01-01

--- src/react_cm/cm01-01_React.js
+++ src/react_cm/cm01-01_React.js
@@ -5,13 +5,16 @@ TODO
  */
 // TODO
 
+import React from 'react';
+// import ReactDOM from 'react-dom/client'; //! ReactDOM.createRoot(document.getElementById("root")).render(image);
+import ReactDOM from 'react-dom'; //! ReactDOM.render(image, document.querySelector("#root"));
+
 /*
 * -
 ! ========================================
  */
-//! ___React-элементы___
 
-// import React from "react";
+//! ___React-элементы___
 
 // const link = React.createElement(
 //     "a",
@@ -22,77 +25,131 @@ TODO
 //     },
 //     "Ссылка на reactjs.org"
 // );
+
+// console.log(link);
 //? ========================================
 
-import React from "react";
 
-const image = React.createElement("img", {
-    src: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
-    alt: "Tacos With Lime",
-    width: 640,
-});
-const title = React.createElement("h2", null, "Tacos With Lime");
-const price = React.createElement("p", null, "Price: 10.99$");
-const button = React.createElement("button", { type: "button" }, "Add to cart");
 
-const product = React.createElement("div", null, image, title, price, button);
-console.log(image);
+// const image = React.createElement("img", {
+//     src: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+//     alt: "Tacos With Lime",
+//     width: 640,
+// });
+// console.log(image);
+
+// const title = React.createElement("h2", null, "Tacos With Lime");
+// console.log(title);
+
+// const price = React.createElement("p", null, "Price: 10.99$");
+// console.log(price);
+
+// const button = React.createElement("button", { type: "button" }, "Add to cart");
+// console.log(button);
+
+// const product = React.createElement("div", null, image, title, price, button);
+// console.log(product);
+
+// /*
+//  * Для передачи детей также используется свойство children параметра props.
+//  * Обратите внимание на то, что свойство children это массив.
+//  */
+// const productWithChildrenInProps = React.createElement("div", {
+//     children: [image, title, price, button],
+// });
+// console.log(productWithChildrenInProps);
 
 /*
- * Для передачи детей также используется свойство children параметра props.
- * Обратите внимание на то, что свойство children это массив.
+* -
+! ========================================
  */
-const productWithChildrenInProps = React.createElement("div", {
-    children: [image, title, price, button],
-});
+
+//! ___Рендер элемента в DOM-дерево___
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(image);
+
+
+
+// ReactDOM.render(image, document.querySelector("#root"));
+
 
 /*
 * -
 ! ========================================
  */
+//! ___JSX___
 
+//* Plain JavaScript
+// const link1 = React.createElement(
+//     "a",
+//     {
+//         href: "https://reactjs.org/",
+//         target: "_blank",
+//         rel: "noreferrer noopener",
+//     },
+//     "Ссылка на reactjs.org"
+// );
+// console.log(link1);
 
+//* JSX
+// const linkWithJSX = (
+//     <a href="https://reactjs.org/" target="_blank" rel="noreferrer noopener">
+//         Ссылка на reactjs.org
+//     </a>
+// );
+// console.log(linkWithJSX);
 
-/*
-* -
-! ========================================
- */
+//? ========================================
+// const imageUrl =
+//     "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+// const price1 = 10.99;
 
-
-
-/*
-* -
-! ========================================
-
-
-
- */
-//! ___Компоненты-функции___
-
-// const MyFunctionalComponent = props => <div>Functional Component</div>;
-
-// const Product = props => (
+// const product1 = (
 //     <div>
-//         <img
-//             src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-//             alt="Tacos With Lime"
-//             width="640"
-//         />
+//         <img src={imageUrl} alt="Tacos With Lime" width="640" />
 //         <h2>Tacos With Lime</h2>
-//         <p>Price: 10.99$</p>
+//         <p>Price: {price1}$</p>
 //         <button type="button">Add to cart</button>
 //     </div>
 // );
+// ReactDOM.render(product1, document.querySelector("#root"));
 
-// // В разметке компонент записывается как JSX-тег
-// ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
+/*
+* -
+! ========================================
+/*
+ */
 
-// console.log(Product);
+//! ___Компоненты-функции___
+
+const MyFunctionalComponent = props => <div>Functional Component</div>;
+
+//? ========================================
+const Product = props => (
+    <div>
+        <img
+            src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+            alt="Tacos With Lime"
+            width="640"
+        />
+        <h2>Tacos With Lime</h2>
+        <p>Price: 12.99$</p>
+        <button type="button">Add to cart</button>
+    </div>
+);
+
+// В разметке компонент записывается как JSX-тег
+ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
+
+console.log(Product);
 
 /*
 * -
 ! ========================================
  */
+
+//! ___Свойства компонента (props)___
 
 // const App = () => (
 //     <>
