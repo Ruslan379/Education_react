17667d23501941fb029d3f21bbfc565b23e4bf35
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-31 14:35:16 +0300

    Finished cm01-1

--- src/react_cm/cm01-1_React.js
+++ src/react_cm/cm01-1_React.js
@@ -202,31 +202,31 @@ import ReactDOM from 'react-dom'; //! 2-й вариант: ReactDOM.render(image
 // const MyFunctionalComponent = props => <div>Functional Component</div>;
 
 //? ========================================
-const Product = props => (
-    <div>
-        <img
-            src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-            alt="Tacos With Lime"
-            width="640"
-        />
-        <h2>Tacos With Lime</h2>
-        <p>Price: 14.99$</p>
-        <button type="button">Add to cart</button>
-    </div>
-);
+// const Product = props => (
+//     <div>
+//         <img
+//             src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//             alt="Tacos With Lime"
+//             width="640"
+//         />
+//         <h2>Tacos With Lime</h2>
+//         <p>Price: 14.99$</p>
+//         <button type="button">Add to cart</button>
+//     </div>
+// );
 
-//! В разметке компонент записывается как JSX-тег
-// ReactDOM.createRoot(document.getElementById("root")).render(<Product />);  //! 1-й вариант
-ReactDOM.render(<Product />, document.querySelector("#root")); //! 2-й вариант
+// //! В разметке компонент записывается как JSX-тег
+// // ReactDOM.createRoot(document.getElementById("root")).render(<Product />);  //! 1-й вариант
+// ReactDOM.render(<Product />, document.querySelector("#root")); //! 2-й вариант
 
-console.log(Product);
+// console.log(Product);
 
 /*
 * -
 ! ========================================
  */
 
-//! ___Свойства компонента (props)___
+//! ___Свойства компонента (props)-1___
 
 // const App = () => (
 //     <>
@@ -235,54 +235,225 @@ console.log(Product);
 //     </>
 // );
 
+// const Product = props => (
+//     <div>
+//         <img src={props.imgUrl} alt={props.name} width="640" />
+//         <h2>{props.name}</h2>
+//         <p>Price: {props.price}$</p>
+//         <button type="button">Add to cart</button>
+//     </div>
+// );
 
+// ReactDOM.render(<App />, document.querySelector("#root")); //! 2-й вариант
 /*
-* - 
+* -
 ! ========================================
  */
 
+//! ___Свойства компонента (props)-2___
+//! ___Значение пропсов по умолчанию___
 
+// const Product = ({
+//     imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+//     name,
+//     price
+// }) => (
+//     <div>
+//         <img src={imgUrl} alt={name} width="640" />
+//         <h2>{name}</h2>
+//         <p>Price: {price}$</p>
+//         <button type="button">Add to cart</button>
+//     </div>
+// );
+
+// const App = () => (
+//     <div>
+//         <h1>Best selling products</h1>
+//         <Product
+//             // imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//             name="Tacos With Lime"
+//             price={9.99}
+//         />
+//         <Product
+//             imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//             name="Fries and Burger"
+//             price={14.29}
+//         />
+//     </div>
+// );
+
+// ReactDOM.render(<App />, document.querySelector("#root")); //! 2-й вариант
 
 /*
-* - 
+* -
 ! ========================================
  */
 
+//! ___Свойство props.children___
 
+// const Profile = ({ name, email }) => (
+//     <div>
+//         <p>Name: {name}</p>
+//         <p>Email: {email}</p>
+//     </div>
+// );
+
+// const Panel = ({ title, children }) => (
+//     <section>
+//         <h2>{title}</h2>
+//         {children}
+//     </section>
+// );
+
+// const App = () => (
+//     <div>
+//         <Panel title="User profile">
+//             <Profile name="Mango" email="mango@mail.com" />
+//         </Panel>
+//     </div>
+// );
+
+// ReactDOM.render(<App />, document.querySelector("#root")); //! 2-й вариант
 
 /*
-* - 
+* -
 ! ========================================
  */
 
+//! ___Рендер по условию___
+//! ___if с помощью логического оператора &&___
 
+// const Mailbox = ({ unreadMessages }) => {
+//     return (
+//         <div>
+//             <h1>Hello!</h1>
+//             {unreadMessages.length > 0 && (
+//                 <p>You have {unreadMessages.length} unread messages.</p> //! Читается как: если условие приводится к true, то рендерим разметку.
+//             )}
+//         </div>
+//     );
+// };
 
 /*
-* - 
+* -
 ! ========================================
  */
 
+//! ___Рендер по условию___
+//! ___if...else с помощью тернарного оператора-1___
+
+// const Mailbox = ({ username, unreadMessages }) => {
+//     return (
+//         <div>
+//             <h1>Hello {username}</h1>
+//             {unreadMessages.length > 0
+//                 ? (<p>You have {unreadMessages.length} unread messages.</p>)
+//                 : (<p>No unread messages.</p>)
+//             }
+//         </div>
+//     );
+// };
+
+//? ============================или так: =========================
+// const Mailbox = ({ name, unreadMessages }) => {
+//     return (
+//         <div>
+//             <h1>Hello {name}</h1>
+//             <p>
+//                 {unreadMessages.length > 0
+//                     ? `You have ${unreadMessages.length} unread messages.`
+//                     : "No unread messages."}
+//             </p>
+//         </div>
+//     );
+// };
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
+//! ___Рендер по условию___
+//! ___if...else с помощью тернарного оператора-2___
 
+// const Product = ({ imgUrl, name, price, quantity }) => (
+//     <div>
+//         <img src={imgUrl} alt={name} width="640" />
+//         <h2>{name}</h2>
+//         <p>Price: {price}$</p>
+//         <h1>Quantity: {quantity < 20 ? "Few left" : "In stock"}</h1>
+//         <button type="button">Add to cart</button>
+//     </div>
+// );
 
 /*
-* - 
+* -
 ! ========================================
  */
 
+//! ___Коллекции + Ключи___
 
+// const favouriteBooks = [
+//     { id: "id-1", name: "JS for beginners" },
+//     { id: "id-2", name: "React basics" },
+//     { id: "id-3", name: "React Router overview" },
+//     { id: "id-4", name: "Redux in depth" },
+// ];
+
+// const BookList = ({ books }) => {
+//     return (
+//         <ul>
+//             {books.map(book => (
+//                 <li key={book.id}>{book.name}</li>
+//             ))}
+//         </ul>
+//     );
+// };
+
+// const App = () => {
+//     return (
+//         <div>
+//             <BookList books={favouriteBooks} />
+//         </div>
+//     );
+// };
+
+// ReactDOM.render(<App />, document.querySelector("#root")); //! 2-й вариант
 
 /*
-* - 
+* -
 ! ========================================
  */
 
+//! ___Коллекции + Ключи - индексы массива.___
+
+const favouriteBooks = [
+    { name: "JS for beginners" },
+    { name: "React basics" },
+    { name: "React Router overview" },
+    { name: "Redux in depth" },
+];
+
+const BookList = ({ books }) => {
+    return (
+        <ul>
+            {books.map(book => (
+                <li key={book.index}>{book.name}</li>
+            ))}
+        </ul>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+            <BookList books={favouriteBooks} />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root")); //! 2-й вариант
 
 
 /*
